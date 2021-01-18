acba8946367e3a944641c79a30b81d5a4f99bd3a
ifreelander <roya.taleghani@gmail.com>
2021-01-19 10:09:13 +1100

    sum of digit of a number using recursion

--- recursion/Recursion/Program.cs
+++ recursion/Recursion/Program.cs
@@ -7,34 +7,21 @@ namespace ConsoleApplication
         public static void  Main()
         {
             var num =-234;
-             //var remainder = num % 10;
-            //var quotient = num / 10;
-            //var sum= 0;
             var sum = SumOfDigitsRecursive(num);
             Console.WriteLine(sum);
-            
-           /* while ( num != 0 )
-            {
-                sum += num % 10;
-                num = num / 10;
-            }*/
-            //return sum;
         }
           public static  int SumOfDigitsRecursive(int num)
           {
               num = Math.Abs(num);
               
-              if (Math.Abs(num) < 10)
+              if (num < 10)
               {
-                  return Math.Abs(num);
+                  return num;
               }
               else
               {
-                  //sum += num % 10;
-                  //sum = sum + (num % 10); //sum = 0 +  3   
-                  return (Math.Abs(num) % 10) +  SumOfDigitsRecursive(Math.Abs(num)/10);
+                  return num % 10 +  SumOfDigitsRecursive(num/10);
               }
           }
-        
     }
 }
