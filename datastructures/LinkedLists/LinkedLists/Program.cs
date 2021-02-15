d341a9d6c729aa74d8b94e519b168d7760f3cd08
roya-myob <roya.taleghani@myob.com>
2021-02-15 21:01:28 +1100

    Add to list func

--- datastructures/LinkedLists/LinkedLists/Program.cs
+++ datastructures/LinkedLists/LinkedLists/Program.cs
@@ -1,62 +1,102 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Diagnostics.Contracts;
+using System.IO.MemoryMappedFiles;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography.X509Certificates;
 
 namespace LinkedLists
+    //Get last element
+    //Add the new node after the last element
+      
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            //var lawrenceAddress = new Address("Pearce St", "B Hills", "2153");
+            
+            var myList = new LinkedList();
 
-            //var new Address("Not my address", lawrenceAddress.Line2, lawrenceAddress.PostCode);
-            //lawrenceAddress.Line1 = "Not my address";
+            myList.Add("A");
+            myList.Add("B");
 
-            //Console.WriteLine(lawrenceAddress.Line1);
-
-            // TODO
-            // Create Node B (points to null)
-
-            // Create Node A pointing to Node B
+            var newLength = myList.GetLength();
+           Console.WriteLine(newLength);
         }
 
     }
-
+  
     class LinkedList
     {
+        private Node head; //head of the list 
+        
+       
 
+        public LinkedList ( )
+        {
+         
+            
+        }
+        
+        
+
+        public void Add(string data)
+        {
+            // create new node
+            // add it to the list
+
+            Node myNode = new Node(data);
+            
+           
+           if(myNode.NextNode == null)
+            {
+                
+                head = myNode;
+                myNode.NextNode = null;
+                
+            }
+            else
+            {
+
+            }
+            
+
+
+        }
+
+        public int GetLength()
+        {
+            // 1- head is our first element and we set current to head .... also we can set count to 1 
+            // 2- we check if there is any element next to the current element, if yes > we increase the count
+            // 3- we need move the current to the next element and go to step 2
+            // if the next is null  then stop the count
+
+            var count = 1;
+            var current = head;
+         
+            while(current.NextNode != null){
+                count++;
+                current = current.NextNode;
+            }
+
+            return count;
+        }
+       
     }
 
     class Node
     {
-        //property - implicit backing field
-        public string Data { get; }
-        public string NextNode { get; }
-
-        public Node(string data, string nextNode)
+        
+        public string Data;
+        public Node NextNode;
+        
+        public Node(string data)
         {
             Data = data;
-            NextNode = nextNode;
+            NextNode = null;
         }
     }
 
-    public class Address
-    {
-        public string Line1 { get; }
-
-        public string Line2 { get; }
-
-        public string PostCode { get; }
-
-        public Address(string line1, string line2, string postCode)
-        {
-            // if line1 contains postcode
-            // { split line1 into line 2 and postcode}
-            // else
-            Line1 = line1;
-            Line2 = line2;
-            PostCode = postCode;
-        }
-    }
+   
 
 }
